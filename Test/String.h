b69d7fb238f9e426a0ff017003f5c9652a885b4f
Gleb Skutin <glebskutin@gmail.com>
2021-06-24 16:45:52 +0500

    Добавил лексикографическое сравнение и вывод входных строк в обратном порядке

--- Test/String.h
+++ Test/String.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <cstring>
+#include <string>
 #pragma warning(disable : 4996) // отключаем предупрждени€ Visual Studio об использовании strcpy
 using namespace std;
 
@@ -9,40 +10,40 @@ class String
 	char* _string;
 	size_t _size;
 public:
-    explicit String(char* str)  //Ѕазовый конструктор
+    explicit String(const char* str)  //Ѕазовый конструктор
     {
-        std::cout << "string constructed " << str << std::endl;
+        //std::cout << "string constructed " << str << std::endl;
         _size = strlen(str) + 1;
         _string = new char[_size];
-        strcpy(_string, str);
+        strcpy_s(_string, _size, str);
     }
     String(const String& other)     //конструктор копировани€
     {
-        std::cout << "string copy constructed " << other._string << std::endl;
-        _string = new char[other._size];
-        strcpy(_string, other._string);
+        //std::cout << "string copy constructed " << other._string << std::endl;
         _size = other._size;
+        _string = new char[_size];
+        strcpy_s(_string, _size, other._string);
     }
     String(String&& other)  noexcept    // онсруктор копировани€ перемещени€
     {
-        std::cout << "move constructor " << other._string << std::endl;
+        //std::cout << "move constructor " << other._string << std::endl;
         _string = other._string;
         _size = other._size;
         other._string = nullptr;
         other._size = 0;
     }
-    String& operator=(const String& other)
+    String& operator=(const String& other) //ќператор копировани€ пристваивани€
     {
-        cout << "string copy constructed " << other._string << endl;
+        //cout << "string copy constructed " << other._string << endl;
         _string = new char[other._size];
-        strcpy(_string, other._string);
+        strcpy_s(_string, _size, other._string);
         _size = other._size;
 
         return *this;
     }
-    String& operator=(String&& other) noexcept  // онструктор присваивани€ перемещени€
+    String& operator=(String&& other) noexcept  //ќператор присваивани€ перемещени€
     {
-        cout << "move assignment constructor " << other._string << endl;
+        //cout << "move assignment constructor " << other._string << endl;
         _string = other._string;
         _size = other._size;
         other._string = nullptr;
@@ -50,6 +51,19 @@ public:
 
         return *this;
     }
+    char& operator[](int index)
+    {
+        if (index < 0 || index > _size)
+        {
+            cout << "Bad Index" << endl;
+        }
+        else
+        {
+            return _string[index];
+        }
+    }
+    const char* GetString() { return _string; }
+    size_t GetSize() { return _size; }
     ~String() noexcept
     {
         delete[] _string;
@@ -61,4 +75,26 @@ ostream& operator<<(ostream& os, const String& str)
 {
     os << str._string;
     return os;
+}
+
+String operator+(String str1, String str2)
+{
+    char* sum = new char[str1.GetSize() + str2.GetSize()];
+    strcpy(sum, str1.GetString());
+    strcat(sum, str2.GetString());
+    return String(sum);
+}
+String operator+(const char* str1, String str2)
+{
+    char* sum = new char[strlen(str1) + str2.GetSize()];
+    strcpy(sum, str1);
+    strcat(sum, str2.GetString());
+    return String(sum);
+}
+String operator+(String str1, const char* str2)
+{
+    char* sum = new char[str1.GetSize() + strlen(str2)];
+    strcpy(sum, str1.GetString());
+    strcat(sum, str2);
+    return String(sum);
 }
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -7,27 +7,101 @@
 #include <string>
 #include <algorithm>
 
-bool cmp(const char* str1, const char* str2)
+bool cmp(String str1, String str2)
 {
-    return std::lexicographical_compare(&str1[0], &str1[strlen(str1)], &str2[0], &str2[strlen(str2)]);
+    char* tmp_str1 = new char[str1.GetSize()+1];
+    char* tmp_str2 = new char[str2.GetSize()+1];
+    bool isless = false;
+    char* add = new char[2];
+    if (str1[0] >= 65 && str1[0] <= 90)
+    {
+        add[0] = str1[0] + 32;
+        add[1] = '\0';
+        strcpy(tmp_str1, add);
+    }
+    else
+    {
+        add[0] = str1[0];
+        add[1] = '\0';
+        strcpy(tmp_str1, add);
+    }
+    for (size_t i = 1; i < str1.GetSize(); i++)
+    {
+        if (str1[i] >= 65 && str1[i] <= 90)
+        {
+            add[0] = str1[i] + 32;
+            add[1] = '\0';
+            strcat(tmp_str1, add);
+        }
+        else
+        {
+            add[0] = str1[i];
+            add[1] = '\0';
+            strcat(tmp_str1, add);
+        }
+    }
+    if (str2[0] >= 65 && str2[0] <= 90)
+    {
+        add[0] = str1[0] + 32;
+        add[1] = '\0';
+        strcpy(tmp_str2, add);
+    }
+    else
+    {
+        add[0] = str2[0];
+        add[1] = '\0';
+        strcpy(tmp_str2, add);
+    }
+    for (size_t i = 1; i < str2.GetSize(); i++)
+    {
+        if (str2[i] >= 65 && str2[i] <= 90)
+        {
+            add[0] = str2[i] + 32;
+            add[1] = '\0';
+            strcat(tmp_str2, add);
+        }
+        else
+        {
+            add[0] = str2[i];
+            add[1] = '\0';
+            strcat(tmp_str2, add);
+        }
+    }
+    isless = std::lexicographical_compare(&tmp_str1[0], &tmp_str1[str1.GetSize()], &tmp_str2[0], &tmp_str2[str2.GetSize()]);
+    delete[] add;
+    delete[] tmp_str1;
+    delete[] tmp_str2;
+    return isless;
 }
-
 int main(int argc, char* argv[])
 {
-    int i = 0;
+    setlocale(LC_ALL, "Russian");
     char f = ' ';
-    std::cout << "Hello World!\n";
-    String str1(argv[0]);
-    cout << str1 << endl;
-    String str2(move(str1));
-    str1 = move(str2);
-
-    vector<const char*> str = { "aa", "aaa", "ab", "aaaa", "abc", "aabbcc" };
-    std::sort(str.begin(), str.end(), cmp);
-    for (auto string : str)
+    vector<String> strings;
+    for (int i = 1; i<argc; i++)
     {
-        std::cout << string << " ";
+        strings.push_back(String(argv[i]));
     }
+    for (auto string : strings)
+    {
+        cout << string << endl;
+    }
+    cout << endl;
+    std::sort(strings.begin(), strings.end(), cmp);
+    for (int i = strings.size() - 1; i >= 0; i--)
+    {
+        cout << strings[i] << endl;
+    }
+    //cout << "Чекаем вариант просто нажать энтер" << endl;
+    /*cin.get(f);
+    if (f == '\n')
+    {
+        cout << "Просто нажать энтер сработало";
+    }
+    else
+    {
+        cout << "Видно не судьба";
+    }*/
 
     /*while (i != argc)
     {
